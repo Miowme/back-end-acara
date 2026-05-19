83ca9161d0cff3e45fda9047b6a5edc86ceef7e7
Miowme <williamsusantolukman@gmail.com>
2026-05-19 19:11:39 +0700

    Setup Database, API Auth and Validation Register User

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
 import express from "express";
 import router from './routes/api';
+import bodyParser from "body-parser";
+import db from "./utils/database";
 
-const app = express();
+async function init() {
+    try {
+        const result = await db();
 
-const PORT = 3000;
+        console.log("database status: ", result);
 
-app.use('/api', router);
+        const app = express();
 
+        const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+        app.use(bodyParser.json());
+
+        app.use('/api', router);
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+init();
