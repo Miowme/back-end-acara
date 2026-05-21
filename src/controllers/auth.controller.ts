b57385d476258e9d4fab2453765ebcd0d0fa7d5b
Miowme <williamsusantolukman@gmail.com>
2026-05-21 13:49:00 +0700

    API For Login, Add Middleware JWT, and API ME

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,9 @@
 import {Request, Response} from 'express';
 import * as Yup from 'yup';
 import UserModel from '../models/user.model';
+import { encrypt } from '../utils/encryption';
+import { generateToken } from '../utils/jwt';
+import { IReqUser } from '../middlewares/auth.middleware';
 
 type TRegister = {
     fullName: string;
@@ -9,6 +12,11 @@ type TRegister = {
     password:string;
     confirmPassword: string;
 };
+
+type TLogin = {
+    identifier: string;
+    password: string;
+}
 
 const registerValidateSchema = Yup.object({
     fullName: Yup.string().required(),
@@ -56,5 +64,72 @@ export default {
             });
         }
 
+    },
+    async login(req: Request, res: Response) {
+
+        const { identifier, password } = req.body as unknown as TLogin;
+
+        try {
+            const userByIdentifier = await UserModel.findOne({
+                $or: [
+                    {
+                        email: identifier,
+                    },
+                    {
+                        username: identifier,
+                    },
+                ],
+            });
+
+            if (!userByIdentifier) {
+                return res.status(403).json({
+                    message: "user not found",
+                    data: null
+                });
+            }
+
+            const validatePassword: boolean = encrypt(password) === userByIdentifier.password;
+
+            if (!validatePassword) {
+                return res.status(403).json({
+                    message: "user not found",
+                    data: null,
+                });
+            }
+
+            const token = generateToken({
+                id: userByIdentifier._id,
+                role: userByIdentifier.role,
+            });
+
+            res.status(200).json({
+                message: "login success",
+                data: token,
+            });
+        } catch (error) {
+            const err = error as unknown as Error;
+            res.status(400).json({
+                message: err.message,
+                data: null,
+            });
+        }
+    },
+
+    async me(req: IReqUser, res: Response) {
+        try {
+            const user = req.user;
+            const result = await UserModel.findById(user?.id);
+
+            res.status(200).json({
+                message: "Success get user profile",
+                data: result,
+            });
+        } catch (error) {
+            const err = error as unknown as Error;
+            res.status(400).json({
+                message: err.message,
+                data: null,
+            });
+        }
     },
 }
